1a6413fb35bb67c4bbf5b7c88db890a6b3dadb83
Hysenbergg <huseyinsaidzeyrek@gmail.com>
2022-11-22 14:43:08 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text} from 'react-native';
+
+const HomePage = () => {
+  return(
+    <SafeAreaView style={{flex: 1}}>
+      <Text>Home Page</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomePage;
